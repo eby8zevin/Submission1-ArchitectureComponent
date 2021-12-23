e4c1157a301557d325c17ee050d00dd64cf340f7
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-23 23:21:54 +0700

    Java MainActivity.java

--- app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ahmadabuhasan.architecturecomponent;
+package com.ahmadabuhasan.architecturecomponent.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ahmadabuhasan.architecturecomponent.R;
 
 public class MainActivity extends AppCompatActivity {
 
