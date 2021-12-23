6c6a9fb3ec97271a75edecc5d1140129ecf92817
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-23 23:22:10 +0700

    Java SplashScreen.java

---
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.ahmadabuhasan.architecturecomponent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+
+import com.ahmadabuhasan.architecturecomponent.ui.MainActivity;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            SplashScreen.this.startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            SplashScreen.this.finish();
+        }, 3000);
+    }
+}
