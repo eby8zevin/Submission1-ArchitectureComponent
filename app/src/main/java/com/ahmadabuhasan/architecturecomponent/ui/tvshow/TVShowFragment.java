7c7d3b0fe0757ac76f57c53ef83223c9cf8a267e
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-26 08:20:01 +0700

    Fragment TVShowFragment.java

---
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/tvshow/TVShowFragment.java
@@ -0,0 +1,31 @@
+package com.ahmadabuhasan.architecturecomponent.ui.tvshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ahmadabuhasan.architecturecomponent.databinding.FragmentTvShowBinding;
+
+public class TVShowFragment extends Fragment {
+
+    public static final String ARG_SECTION_NUMBER = "section_number";
+
+    FragmentTvShowBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentTvShowBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
