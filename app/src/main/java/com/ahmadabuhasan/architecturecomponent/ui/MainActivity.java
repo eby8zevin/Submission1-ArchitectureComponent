d70e9949167ecd28ef7778aa45a737a4da1dddbf
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-26 08:21:29 +0700

    Added Fragment

--- app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/MainActivity.java
@@ -1,16 +1,44 @@
 package com.ahmadabuhasan.architecturecomponent.ui;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.ahmadabuhasan.architecturecomponent.R;
+import com.ahmadabuhasan.architecturecomponent.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    @StringRes
+    private final int[] TAB_TITLES = new int[]{
+            R.string.movies,
+            R.string.tv_show
+    };
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(this);
+        ViewPager2 viewPager2 = binding.viewPager;
+        viewPager2.setAdapter(mainPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        new TabLayoutMediator(tabs, viewPager2,
+                (tab, position) -> tab.setText(getResources().getString(TAB_TITLES[position]))
+        ).attach();
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
     }
 }
