e7fb0b6f76f958925ffbd20ec519b1b9ac7906c2
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-26 08:20:18 +0700

    Fragment MovieFragment.java

---
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/movies/MovieFragment.java
@@ -0,0 +1,31 @@
+package com.ahmadabuhasan.architecturecomponent.ui.movies;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ahmadabuhasan.architecturecomponent.databinding.FragmentMoviesBinding;
+
+public class MovieFragment extends Fragment {
+
+    public static final String ARG_SECTION_NUMBER = "section_number";
+
+    FragmentMoviesBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentMoviesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
