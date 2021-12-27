fc9b812863f38f709d689adb9a6ccafe1a91cfd6
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-27 19:23:39 +0700

    Data

---
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/utils/DataDummy.java
@@ -0,0 +1,18 @@
+package com.ahmadabuhasan.architecturecomponent.utils;
+
+import com.ahmadabuhasan.architecturecomponent.data.MovieEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataDummy {
+
+    public static List<MovieEntity> generateDummyMovie() {
+        ArrayList<MovieEntity> movie = new ArrayList<MovieEntity>();
+
+        movie.add(new MovieEntity(
+
+        ));
+        return movie;
+    }
+}
