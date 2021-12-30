1b40424f2299055ac5c35b8ed2e365bbe5739e3f
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-31 06:11:23 +0700

    Movie Detail

---
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/movies/MovieDetail.java
@@ -0,0 +1,21 @@
+package com.ahmadabuhasan.architecturecomponent.ui.movies;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ahmadabuhasan.architecturecomponent.R;
+import com.ahmadabuhasan.architecturecomponent.databinding.ActivityMovieDetailBinding;
+
+public class MovieDetail extends AppCompatActivity {
+
+    public static final String EXTRA_MOVIE = "extra_movie";
+
+    private ActivityMovieDetailBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
