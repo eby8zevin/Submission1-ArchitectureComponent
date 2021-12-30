ad757c017b39a31a972af42ad093b0a71ca51c7f
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-31 06:11:39 +0700

    TVShow Adapter

--- app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/tvshows/TVShowAdapter.java
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/tvshows/TVShowAdapter.java
@@ -1,14 +1,17 @@
 package com.ahmadabuhasan.architecturecomponent.ui.tvshows;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ahmadabuhasan.architecturecomponent.R;
 import com.ahmadabuhasan.architecturecomponent.data.TVShowEntity;
 import com.ahmadabuhasan.architecturecomponent.databinding.ItemListBinding;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +38,16 @@ public class TVShowAdapter extends RecyclerView.Adapter<TVShowAdapter.TVShowView
         TVShowEntity tvShowEntity = listTVShow.get(position);
         Glide.with(holder.itemView.getContext())
                 .load(tvShowEntity.getPoster())
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_error))
                 .into(holder.binding.ivPoster);
         holder.binding.tvTitle.setText(tvShowEntity.getTitle());
         holder.binding.tvRelease.setText(tvShowEntity.getRelease());
+
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), TVShowDetail.class);
+            intent.putExtra(TVShowDetail.EXTRA_TVSHOW, tvShowEntity.getTvshowId());
+            view.getContext().startActivity(intent);
+        });
     }
 
     @Override
