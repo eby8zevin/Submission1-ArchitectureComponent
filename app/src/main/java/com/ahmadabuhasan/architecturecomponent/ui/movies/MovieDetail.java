9984b577ed66fc4196584cd0dc8e78771c85ba4d
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-12-31 21:42:32 +0700

    Movie Detail

--- app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/movies/MovieDetail.java
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/movies/MovieDetail.java
@@ -3,9 +3,14 @@ package com.ahmadabuhasan.architecturecomponent.ui.movies;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.ahmadabuhasan.architecturecomponent.R;
+import com.ahmadabuhasan.architecturecomponent.data.MovieEntity;
 import com.ahmadabuhasan.architecturecomponent.databinding.ActivityMovieDetailBinding;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.Objects;
 
@@ -18,8 +23,36 @@ public class MovieDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_detail);
-        setTitle("Detail Movie");
+        //setContentView(R.layout.activity_movie_detail);
+
+        binding = ActivityMovieDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        MovieDetailViewModel viewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MovieDetailViewModel.class);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String movieId = extras.getString(EXTRA_MOVIE);
+            if (movieId != null) {
+                viewModel.setSelectedMovie(movieId);
+                populateMovie(viewModel.getMovie());
+            }
+        }
+
+        binding.toolbar.setNavigationOnClickListener(view -> onBackPressed());
+    }
+
+    private void populateMovie(MovieEntity movieEntity) {
+        binding.collapsing.setTitle(movieEntity.getTitle());
+        binding.tvGenreDuration.setText(String.format("%s  •  %s", movieEntity.getGenre(), movieEntity.getDuration()));
+        binding.tvDetailOverview.setText(movieEntity.getOverview());
+
+        Glide.with(this)
+                .load(movieEntity.getPoster())
+                .transform(new RoundedCorners(20))
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading).error(R.drawable.ic_broken_image))
+                .into(binding.ivDetail);
     }
 }
