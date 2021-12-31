316f79b19aea36a1371f6c7b8ee7ba1e490902d9
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-01 02:58:39 +0700

    Move Variable View Binding

--- app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/MainActivity.java
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ahmadabuhasan.architecturecomponent.ui;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,14 +18,15 @@ public class MainActivity extends AppCompatActivity {
             R.string.tv_show
     };
 
-    private ActivityMainBinding binding;
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         MainPagerAdapter mainPagerAdapter = new MainPagerAdapter(this);
@@ -36,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setElevation(0);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 3000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
     }
 }
