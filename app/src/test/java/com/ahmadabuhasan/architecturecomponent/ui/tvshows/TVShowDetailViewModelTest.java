abc12c75b83c236e39d6ae301dc8b555efb5c49a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-02 10:02:56 +0700

    TVShowDetailViewModelTest

---
+++ app/src/test/java/com/ahmadabuhasan/architecturecomponent/ui/tvshows/TVShowDetailViewModelTest.java
@@ -0,0 +1,37 @@
+package com.ahmadabuhasan.architecturecomponent.ui.tvshows;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.ahmadabuhasan.architecturecomponent.data.TVShowEntity;
+import com.ahmadabuhasan.architecturecomponent.utils.DataDummy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TVShowDetailViewModelTest {
+
+    private TVShowDetailViewModel viewModel;
+    private TVShowEntity dummyTVShow = DataDummy.generateDummyTVShow().get(0);
+    private String tvshowId = dummyTVShow.getTvshowId();
+
+    @Before
+    public void setUp() {
+        viewModel = new TVShowDetailViewModel();
+        viewModel.setSelectedTVShow(tvshowId);
+    }
+
+    @Test
+    public void testSetSelectedTVShow() {
+        viewModel.setSelectedTVShow(dummyTVShow.getTvshowId());
+        TVShowEntity tvShowEntity = viewModel.getTVShow();
+        assertNotNull(tvShowEntity);
+        assertEquals(dummyTVShow.getTvshowId(), tvShowEntity.getTvshowId());
+        assertEquals(dummyTVShow.getTitle(), tvShowEntity.getTitle());
+        assertEquals(dummyTVShow.getRelease(), tvShowEntity.getRelease());
+        assertEquals(dummyTVShow.getGenre(), tvShowEntity.getGenre());
+        assertEquals(dummyTVShow.getDuration(), tvShowEntity.getDuration());
+        assertEquals(dummyTVShow.getOverview(), tvShowEntity.getOverview());
+        assertEquals(dummyTVShow.getPoster(), tvShowEntity.getPoster());
+    }
+}
