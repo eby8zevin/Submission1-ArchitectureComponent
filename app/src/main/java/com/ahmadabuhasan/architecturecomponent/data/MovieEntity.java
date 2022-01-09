e7c54d3d98c6a003800ba9a429e770e20a5fcf7c
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-01-09 23:25:18 +0700

    Analyze - Code Cleanup

--- app/src/main/java/com/ahmadabuhasan/architecturecomponent/data/MovieEntity.java
+++ app/src/main/java/com/ahmadabuhasan/architecturecomponent/data/MovieEntity.java
@@ -24,56 +24,28 @@ public class MovieEntity {
         return movieId;
     }
 
-    public void setMovieId(String movieId) {
-        this.movieId = movieId;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getRelease() {
         return release;
     }
 
-    public void setRelease(String release) {
-        this.release = release;
-    }
-
     public String getGenre() {
         return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
     }
 
     public String getDuration() {
         return duration;
     }
 
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
-
     public String getOverview() {
         return overview;
     }
 
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
     public Integer getPoster() {
         return poster;
-    }
-
-    public void setPoster(Integer poster) {
-        this.poster = poster;
     }
 
 }
